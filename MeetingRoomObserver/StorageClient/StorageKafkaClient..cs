6cd831ac48ae5511b55ff77c9e7d373e71c81f2b
Janne Salonen <janne.salonen@avanio.com>
2023-01-19 09:41:23 +0200

    kafka producer config moved to kafkaclientfactory

--- MeetingRoomObserver/StorageClient/StorageKafkaClient..cs
+++ MeetingRoomObserver/StorageClient/StorageKafkaClient..cs
@@ -1,5 +1,6 @@
 using Confluent.Kafka;
 using MeetingRoomObserver.StorageClient.DTOs;
+using MeetingRoomObserver.Events.Providers;
 using Newtonsoft.Json;
 
 namespace MeetingRoomObserver.StorageClient
@@ -14,63 +15,29 @@ namespace MeetingRoomObserver.StorageClient
         private readonly IConfiguration _configuration;
         private readonly ILogger<StorageKafkaClient> _logger;
         private IHostEnvironment _hostEnvironment;
+        private IKafkaClientFactory _clientFactory;
 
-        public StorageKafkaClient(IConfiguration configuration, IHostEnvironment hostEnvironment, ILogger<StorageKafkaClient> logger)
+        public StorageKafkaClient(
+            IConfiguration configuration,
+            IHostEnvironment hostEnvironment,
+            ILogger<StorageKafkaClient> logger,
+            IKafkaClientFactory clientFactory)
         {
             _configuration = configuration;
             _hostEnvironment = hostEnvironment;
             _logger = logger;
+            _clientFactory = clientFactory;
         }
 
         public Task SendEvent(StorageEventDTO storageEventDTO)
         {
             _logger.LogInformation("Sending event to Storage");
-            var topic = _configuration["KAFKA_TOPIC"];
+            var topic = _configuration["KAFKA_PRODUCER_TOPIC"];
 
-            var producer = CreateKafkaProducer();
+            var producer = _clientFactory.CreateProducer();
             var jsonBody = JsonConvert.SerializeObject(storageEventDTO);
 
             return producer.ProduceAsync(topic, new Message<Null, string> { Value = jsonBody });
-        }
-
-        private ProducerConfig CreateKafkaConfiguration()
-        {
-            if (_hostEnvironment.IsDevelopment())
-            {
-                return new ProducerConfig
-                {
-                    BootstrapServers = _configuration["KAFKA_BOOTSTRAP_SERVER"],
-                };
-            }
-
-            var cert = parseCert(_configuration["SSL_CERT_PEM"]);
-
-            return new ProducerConfig
-            {
-                BootstrapServers = _configuration["KAFKA_BOOTSTRAP_SERVER"],
-                SaslMechanism = SaslMechanism.ScramSha512,
-                SecurityProtocol = SecurityProtocol.SaslSsl,
-                SaslUsername = _configuration["KAFKA_USER_USERNAME"],
-                SaslPassword = _configuration["KAFKA_USER_PASSWORD"],
-                SslCertificatePem = cert,
-            };
-        }
-
-        private IProducer<Null, string> CreateKafkaProducer()
-        {
-            var config = CreateKafkaConfiguration();
-
-            return new ProducerBuilder<Null, string>(config).Build();
-        }
-
-        private string parseCert(string cert)
-        {
-            cert = cert.Replace("\"", "");
-
-            var certBegin = "-----BEGIN CERTIFICATE-----\n";
-            var certEnd = "\n-----END CERTIFICATE-----";
-
-            return certBegin + cert + certEnd;
         }
 
     }
