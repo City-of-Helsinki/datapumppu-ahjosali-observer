6daf1f35272211e738a4c061cabbb1383320152b
Ville Palo <ville.palo@avanio.com>
2023-03-17 16:03:04 +0200

    Added some logging

--- MeetingRoomObserver/MeetingMessageHandler.cs
+++ MeetingRoomObserver/MeetingMessageHandler.cs
@@ -11,6 +11,7 @@ namespace MeetingRoomObserver
 
     public class MeetingMessageHandler : IMeetingMessageHandler
     {
+        private readonly ILogger<MeetingMessageHandler> _logger;
         private readonly IMeetingEventParser _meetingEventParser;
         private readonly IStorageDTOMapper _storageDTOMapper;
         private readonly IStorage _storage;
@@ -18,15 +19,18 @@ namespace MeetingRoomObserver
         public MeetingMessageHandler(
             IMeetingEventParser meetingEventParser,
             IStorageDTOMapper inputDtoToOutputDTOMapper,
-            IStorage storage)
+            IStorage storage,
+            ILogger<MeetingMessageHandler> logger)
         {
             _meetingEventParser = meetingEventParser;
             _storageDTOMapper = inputDtoToOutputDTOMapper;
             _storage = storage;
+            _logger = logger;
         }
 
         public async Task HandleMessage(string jsonBody)
         {
+            _logger.LogInformation("HandleMessage: " + jsonBody);
             var meetingEvents = _meetingEventParser.ParseJsonMessage(jsonBody);
 
             var storageEventList = _storageDTOMapper.MapToStorageDTOs(meetingEvents);
